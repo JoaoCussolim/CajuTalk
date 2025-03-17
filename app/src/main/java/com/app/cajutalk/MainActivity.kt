fdebf1647a15611f0882f42cfaf8b2b7fcc37456
JoaoCussolim <joaovccussolim@gmail.com>
2025-03-17 11:33:30 -0300

    Login e Cadastro
    Criado as telas de login e cadastro, com funcionalidade de rota entre elas

--- app/src/main/java/com/app/cajutalk/MainActivity.kt
+++ app/src/main/java/com/app/cajutalk/MainActivity.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.material3.OutlinedTextFieldDefaults
-
+import androidx.compose.ui.text.style.TextAlign
 
 
 class MainActivity : ComponentActivity() {
@@ -42,18 +42,13 @@ class MainActivity : ComponentActivity() {
 fun CajuTalkApp() {
     val navController = rememberNavController()
 
-    NavHost(navController, startDestination = "cadastro") {
+    NavHost(navController, startDestination = "login") {
         composable("login") { LoginScreen(navController) }
         composable("cadastro") { CadastroScreen(navController) }
         composable("salas") { SalasScreen(navController) }
         composable("criar_sala") { CriarSalaScreen(navController) }
         composable("chat") { ChatScreen(navController) }
     }
-}
-
-@Composable
-fun LoginScreen(navController: NavController) {
-
 }
 
 @Composable
@@ -77,6 +72,156 @@ fun WaveBackground(color: Color, modifier: Modifier = Modifier) {
         }
 
         drawPath(path = path, color = color)
+    }
+}
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    val topColor = Color(0xFFFFD779)
+    val bottomColor = Color(0xFFFDB361)
+    val waveColor = Color(0xFFFFA95C)
+    val accentColor = Color(0xFFFF6F9C)
+    val highlightColor = Color(0xFFFF7D4C)
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(topColor, bottomColor),
+                    startY = 0f,
+                    endY = Float.POSITIVE_INFINITY
+                )
+            )
+    ) {
+
+        WaveBackground(
+            color = waveColor,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Text(
+            text = "CajuTalk",
+            fontSize = 70.sp,
+            fontFamily = FontFamily(Font(R.font.baloo_bhai)),
+            fontWeight = FontWeight(400),
+            color = Color(0xE5FFFAFA),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 60.dp)
+        )
+
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            modifier = Modifier
+                .fillMaxWidth(0.8f) // Deixa mais estreito
+                .align(Alignment.Center)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 40.sp,
+                    color = highlightColor,
+                    fontWeight = FontWeight(400),
+                    fontFamily = FontFamily(Font(R.font.anton))
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = {
+                        Text(
+                            text = "Nome de usuário",
+                            fontSize = 15.sp,
+                            fontFamily = FontFamily(Font(R.font.lexend)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFFF08080),
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = accentColor,
+                        cursorColor = accentColor
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = {
+                        Text(
+                            text = "Senha",
+                            fontSize = 15.sp,
+                            fontFamily = FontFamily(Font(R.font.lexend)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFFF08080),
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = accentColor,
+                        cursorColor = accentColor
+                    ),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        // Lógica de login
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(45.dp),
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = accentColor,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "Login",
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFFFFE4EB),
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Ainda não possui uma conta? ")
+                    Text(
+                        text = "Cadastre-se",
+                        fontSize = 15.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        color = accentColor,
+                        modifier = Modifier.clickable {
+                            navController.navigate("cadastro")
+                        }
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -146,7 +291,15 @@ fun CadastroScreen(navController: NavController) {
                 OutlinedTextField(
                     value = username,
                     onValueChange = { username = it },
-                    label = { Text("Nome de usuário") },
+                    label = {
+                        Text(
+                            text = "Nome de usuário",
+                            fontSize = 15.sp,
+                            fontFamily = FontFamily(Font(R.font.lexend)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFFF08080),
+                        )
+                            },
                     modifier = Modifier.fillMaxWidth(),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = accentColor,
@@ -159,7 +312,15 @@ fun CadastroScreen(navController: NavController) {
                 OutlinedTextField(
                     value = password,
                     onValueChange = { password = it },
-                    label = { Text("Senha") },
+                    label = {
+                        Text(
+                            text = "Senha",
+                            fontSize = 15.sp,
+                            fontFamily = FontFamily(Font(R.font.lexend)),
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFFF08080),
+                        )
+                            },
                     modifier = Modifier.fillMaxWidth(),
                         colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = accentColor,
@@ -173,7 +334,15 @@ fun CadastroScreen(navController: NavController) {
                 OutlinedTextField(
                     value = confirmPassword,
                     onValueChange = { confirmPassword = it },
-                    label = { Text("Confirme a senha") },
+                    label = {
+                        Text(
+                        text = "Confirme a senha",
+                        fontSize = 15.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFFF08080),
+                        )
+                            },
                     modifier = Modifier.fillMaxWidth(),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = accentColor,
@@ -188,25 +357,35 @@ fun CadastroScreen(navController: NavController) {
                     onClick = {
                         // Lógica de cadastro
                     },
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(45.dp),
 
                     colors = ButtonDefaults.buttonColors(
                         containerColor = accentColor,
                         contentColor = Color.White
                     )
                 ) {
-                    Text(text = "Cadastrar")
+                    Text(
+                        text = "Cadastrar",
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFFFFE4EB),
+                    )
                 }
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Row(
+                Column(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(text = "Já possui uma conta? ")
+                    Text(text = "Já possui uma conta? ",)
                     Text(
                         text = "Faça login",
+                        fontSize = 15.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
                         color = accentColor,
                         modifier = Modifier.clickable {
                             navController.navigate("login")
